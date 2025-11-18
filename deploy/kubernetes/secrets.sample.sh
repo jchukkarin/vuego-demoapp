cb46e4372f50d52ca7e455e693d0171ecd8c98d8
jchukkarin <jchukkarin@gmail.com>
2025-11-18 15:55:41 +0700

    first_vue

--- deploy/kubernetes/secrets.sample.sh
+++
@@ -1,1 +0,0 @@
-kubectl create secret generic vuego-demoapp --from-env-file=../../server/.env
--- frontend/babel.config.js
+++
@@ -1,4 +0,0 @@
-// This is only here for the tests, so jest can handle .vue files
-module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset']
-}
